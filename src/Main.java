6bfb8f9e8e191d3c250209b3b87fb17038a3b934
tiaogosilva28 <tiagosilva28@live.com.pt>
2023-01-31 18:07:55 +0000

    commit with new stuff on remove

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       ToDoList toDoList = new ToDoList("Tarefas Domésticas");
-       ToDoList.TodoItem todoItem = toDoList.new TodoItem();
-
+       ToDoList myList = new ToDoList("Tarefas Domésticas");
+       ToDoList.TodoItem todoItem = myList.new TodoItem();
        todoItem.createStreams();
-       
-        System.out.println(toDoList.tasksList.get(0).taskDescription);
+
+
+        System.out.println(myList.tasksList.get(0).taskDescription);
     }
 }
