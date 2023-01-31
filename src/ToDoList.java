b3b9c033b464f1e6f82a7fdbe8f4524596df7e92
tiaogosilva28 <tiagosilva28@live.com.pt>
2023-01-31 15:23:10 +0000

    create empty classes

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,2 +1,27 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class ToDoList {
+    String name;
+    Queue<Integer> priorityQueue = new PriorityQueue<>();
+
+    public class TodoItem{
+        String taskName;
+        String taskDescription;
+        boolean isComplete;
+        int priorityLevel;
+
+        void addTask(TodoItem task) {}
+        void removeTask(TodoItem task){}
+        void getNextTask() {}
+        void getAllTasks(){}
+        void getIncompleteTasks(){}
+        void getCompleteTasks(){}
+        void markTaskComplete(TodoItem task){}
+        void markTaskIncomplete(TodoItem task){}
+
+
+
+
+    }
 }
