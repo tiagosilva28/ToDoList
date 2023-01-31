f674736c2181d3b7c1e2c7792b2a0da204e6e461
tiaogosilva28 <tiagosilva28@live.com.pt>
2023-01-31 16:03:49 +0000

    Create classes of Streams, Input and addTask

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+       ToDoList toDoList = new ToDoList();
+       ToDoList.TodoItem todoItem = toDoList.new TodoItem();
+
+       todoItem.createStreams();
+       todoItem.addTask();
+
+        System.out.println(toDoList.tasks.get(0).taskDescription);
     }
 }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,9 +1,24 @@
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ToDoList {
     String name;
     Queue<Integer> priorityQueue = new PriorityQueue<>();
+    ArrayList<TodoItem> tasks;
+    private BufferedReader consoleReader;
+
+
+    public ToDoList() {
+        this.name = "Lista 1";
+        this.priorityQueue = new PriorityQueue<>();
+        this.tasks = new ArrayList<TodoItem>();
+    }
+
+
+
 
     public class TodoItem{
         String taskName;
@@ -11,14 +26,53 @@ public class ToDoList {
         boolean isComplete;
         int priorityLevel;
 
-        void addTask(TodoItem task) {}
-        void removeTask(TodoItem task){}
-        void getNextTask() {}
-        void getAllTasks(){}
-        void getIncompleteTasks(){}
-        void getCompleteTasks(){}
-        void markTaskComplete(TodoItem task){}
-        void markTaskIncomplete(TodoItem task){}
+        public TodoItem() {
+        }
+
+        public TodoItem(String taskName, String taskDescription, int priorityLevel) {
+            this.taskName = taskName;
+            this.taskDescription = taskDescription;
+            this.isComplete = false;
+            this.priorityLevel = priorityLevel;
+        }
+
+        public void createStreams() throws IOException {
+            consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String userAskInput(String message) throws IOException {
+
+            System.out.println(message);
+            return consoleReader.readLine();
+        }
+
+        void addTask() throws IOException {
+            this.taskName = userAskInput(Messages.TASK_NAME);
+            this.taskDescription = userAskInput(Messages.TASK_DESCRIPTION);
+            this.priorityLevel = Integer.parseInt(userAskInput(Messages.TASK_PRIORITY_LEVEL));
+            tasks.add(new TodoItem(this.taskName,this.taskDescription,this.priorityLevel));
+        }
+        void removeTask(TodoItem task){
+
+        }
+        void getNextTask() {
+
+        }
+        void getAllTasks(){
+
+        }
+        void getIncompleteTasks(){
+
+        }
+        void getCompleteTasks(){
+
+        }
+        void markTaskComplete(TodoItem task){
+
+        }
+        void markTaskIncomplete(TodoItem task){
+
+        }
 
 
 
